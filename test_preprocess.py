c958e912305b14d3d7bd4766ecd6d6e8cd3b26c7
Reberac Aurélie <aurelie.reberac@latmos.ipsl.fr>
2016-05-25 17:26:37 +0200

    add comment for travis test

--- test_preprocess.py
+++ test_preprocess.py
@@ -3,7 +3,7 @@ from numpy.testing.utils import assert_almost_equal
 
 import preprocess
 
-
+# special comment for travis test
 def test_center():
     # After centering, mean should be the desired mean and the
     # standard deviation should be unchanged
